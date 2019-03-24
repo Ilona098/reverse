117fab7ab2c8134af0fa4dd5eb93001f756adcb2
Ilona Lange <ilona.lange098@gmail.com>
2019-03-24 09:50:51 +0000

    Program to reverse a given number, word or sentence

---
+++ Reverse.py
@@ -0,0 +1,11 @@
+# # This is a Python Program to reverse a given number, word or sentence.
+
+n = input("Input a number word or sentence: ")
+
+n = list(reversed(n))
+
+n = ''.join(n)
+
+print("Your input in reverse is: ", n)
+
+
